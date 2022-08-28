e41cdf4ec2bcbfc2ee0a0637ed8ed376016f950c
fangfelipe <fangfelipe@gmail.com>
2022-08-28 09:12:24 +0800

    I dont konw what it is

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,29 @@
 
+
+# usernameInputElement = driver.find_element(By.NAME, 'username')
+# usernameInputElement.clear()
+# usernameInputElement.send_keys("testDanFang")
+
+# passwordInputElement = driver.find_element(By.NAME, 'password')
+# passwordInputElement.clear()
+# passwordInputElement.send_keys("D12345678")
+# loginButton = driver.find_element(
+#     By.XPATH, '//*[@id="loginForm"]/div/div[3]/button')
+# # loginButton.click()
+
+
 # WebDriverWait(driver, 10).until(
 #     EC.presence_of_element_located(
-#         (By.XPATH, '//*[@id="loginForm"]/div/div[3]'))
+#         (By.CLASS_NAME, "cTBqC")
+#     )
 # )
+# searchElement = driver.find_element(By.CLASS_NAME, "cTBqC")
+# searchElement.click()
+# searchInputElement = driver.find_element(By.CLASS_NAME, "XTCLo")
+# searchInputElement.send_keys("新竹美食")
+
+# WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located(
+#         (By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div'))
+# )
+# searchInputElement.send_keys(Keys.RETURN)
