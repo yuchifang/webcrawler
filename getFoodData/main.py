10f3d62e39a7125a72b314d2dd943c359598c17d
fangfelipe <fangfelipe@gmail.com>
2022-08-29 23:07:35 +0800

    update

--- getFoodData/main.py
+++ getFoodData/main.py
@@ -8,13 +8,16 @@ from utils import waitUntil
 import data
 import pathlib
 
-currentFolderPath = pathlib.Path(__file__).parent.resolve() 
+currentFolderPath = pathlib.Path(__file__).parent.resolve()
 # todo test window
+print(currentFolderPath)
 
 service = ChromeService(
-    executable_path= str(currentFolderPath) + '/chromedriver_linux64/chromedriver' # linux
-    #executable_path='D:\work\python\webcrawlerBasic\chromedriver_win32\chromedriver.exe' # window
-    )
+    # executable_path= str(currentFolderPath) + '/chromedriver_linux64/chromedriver' # linux
+    # window
+    executable_path=str(currentFolderPath) + \
+    '\chromedriver_win32\chromedriver.exe'
+)
 
 
 driver = webdriver.Chrome(service=service)
@@ -35,27 +38,13 @@ driver.add_cookie({
 
 driver.refresh()
 
-# searchElement = waitUntil(driver, 10, By.CLASS_NAME, '_a9-- _a9_1')
-
-try:   
-    # element = driver.find_element(By.CLASS_NAME,"_a9-- _a9_1").size()
+try:
     searchElement = waitUntil(driver, 10, By.CLASS_NAME, "_a9-- _a9_1")
 except TimeoutException:
     try:
-        lightboxElement = driver.find_element(By.XPATH,'//*[@id="scrollview"]/following-sibling::div')
-        # lightboxElement = waitUntil(driver,10,By.XPATH,'//*[@id="scrollview"]')
-        print("-----------------------")
-        print(lightboxElement.tag_name)
-        print("-----------------------")
-        print(lightboxElement.text)
-        print("-----------------------")
-        print("-----------------------")
-        driver.switch_to.frame(lightboxElement)
-        testText=lightboxElement.find_element(By.XPATH,'//*[@id="scrollview"]/div/div/div/div[1]/div[1]/section/main/div[1]/section/div/div[2]/h4')
-        print(testText.text)
-        # driver.switch_to.window()
-        # todo 抓抓看 純 div
-        # //*[@id="mount_0_0_zF"]/div/div/div/div[2]
+        lightboxElement = driver.find_element(
+            By.XPATH, '//*[@id="scrollview"]/following-sibling::div/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]')
+
     except NoSuchElementException:
         print("some")
     except TimeoutException:
@@ -64,7 +53,7 @@ except NoSuchElementException:
     print("abv")
 
 # 重新整理
-# todo 感覺抓不到 lightbox html 
+# todo 感覺抓不到 lightbox html
 
 # 等待 如果有取到某個值 就繼續下去
 
