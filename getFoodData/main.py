18cd5257505583e557338d1064bead0f660b75b0
yuchifang <Dan.Fang@gmail.com>
2022-08-30 15:29:22 +0800

    update main.py

--- getFoodData/main.py
+++ getFoodData/main.py
@@ -4,20 +4,24 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+
 from utils import waitUntil
+from login import login
+
 import data
 import pathlib
+import platform
 
 currentFolderPath = pathlib.Path(__file__).parent.resolve()
-# todo test window
-print(currentFolderPath)
+MY_OS = platform.system()
+if MY_OS == "Linux":
+    path = str(currentFolderPath) + '/chromedriver_linux64/chromedriver' 
+else:
+    path = str(currentFolderPath) + '\chromedriver_win32\chromedriver.exe' 
 
-service = ChromeService(
-    # executable_path= str(currentFolderPath) + '/chromedriver_linux64/chromedriver' # linux
-    # window
-    executable_path=str(currentFolderPath) + \
-    '\chromedriver_win32\chromedriver.exe'
-)
+service = ChromeService(executable_path = path )
+
 
 
 driver = webdriver.Chrome(service=service)
@@ -39,18 +43,19 @@ driver.add_cookie({
 driver.refresh()
 
 try:
-    searchElement = waitUntil(driver, 10, By.CLASS_NAME, "_a9-- _a9_1")
-except TimeoutException:
-    try:
-        lightboxElement = driver.find_element(
-            By.XPATH, '//*[@id="scrollview"]/following-sibling::div/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]')
+    lightboxElement = waitUntil(driver,10,By.XPATH,'//*[@id="scrollview"]/following-sibling::div/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]') 
+    lightboxElement.click()
+except TimeoutException or NoSuchElementException:
+    login(driver)
 
-    except NoSuchElementException:
-        print("some")
-    except TimeoutException:
-        print("TimeError")
-except NoSuchElementException:
-    print("abv")
+searchElemnt = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]')
+searchElemnt.click()
+searchInputElement = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]/input')
+searchInputElement.send_keys("新竹美食")
+print(searchInputElement.tag_name)
+sleep(1) # 改成 抓 List 的資料
+searchInputElement.send_keys(Keys.RETURN)
+searchInputElement.send_keys(Keys.ENTER)
 
 # 重新整理
 # todo 感覺抓不到 lightbox html
