19411fdea9744671485e62cc73b3667a09e38574
fangfelipe <fangfelipe@gmail.com>
2022-09-04 11:20:42 +0800

    add prettier

--- getFoodData/main.py
+++ getFoodData/main.py
@@ -1,4 +1,3 @@
-from os import waitid_result
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -14,15 +13,16 @@ import data
 import pathlib
 import platform
 
+print("some", __file__)
+
 currentFolderPath = pathlib.Path(__file__).parent.resolve()
 MY_OS = platform.system()
 if MY_OS == "Linux":
-    path = str(currentFolderPath) + '/chromedriver_linux64/chromedriver' 
+    path = str(currentFolderPath) + '/chromedriver_linux64/chromedriver'
 else:
-    path = str(currentFolderPath) + '\chromedriver_win32\chromedriver.exe' 
+    path = str(currentFolderPath) + '\chromedriver_win32\chromedriver.exe'
 
-service = ChromeService(executable_path = path )
-
+service = ChromeService(executable_path=path)
 
 
 driver = webdriver.Chrome(service=service)
@@ -45,10 +45,11 @@ driver.refresh()
 
 try:
     # 找到 lightbox 並點擊關閉按鈕
-    # lightboxElement = waitUntil(driver,10,By.XPATH,'//*[@id="scrollview"]/following-sibling::div/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]') 
+    # lightboxElement = waitUntil(driver,10,By.XPATH,'//*[@id="scrollview"]/following-sibling::div/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]')
     # lightboxElement.click()
     # 找到 lightbox 刪除
-    lightboxElement = waitUntil(driver,10,By.XPATH,'/html/body/div[1]/div/div/div')
+    lightboxElement = waitUntil(
+        driver, 10, By.XPATH, '/html/body/div[1]/div/div/div')
     driver.execute_script('''
         lightboxElement=document.querySelector("#scrollview").nextElementSibling
         lightboxElement.parentElement.removeChild(lightboxElement)
@@ -56,12 +57,12 @@ try:
 except TimeoutException or NoSuchElementException:
     login(driver)
 
-searchElemnt = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]')
+searchElemnt = driver.find_element(
+    By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]')
 searchElemnt.click()
-searchInputElement = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]/input')
+searchInputElement = driver.find_element(
+    By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]/input')
 searchInputElement.send_keys("新竹美食")
 sleep(1)
 searchInputElement.send_keys(Keys.RETURN)
 searchInputElement.send_keys(Keys.ENTER)
-
-
