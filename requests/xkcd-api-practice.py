37eaa0620c32c57565e12a54b5e252b39a12f565
fangfelipe <fangfelipe@gmail.com>
2022-09-04 11:22:03 +0800

    complete test

--- requests/xkcd-api-practice.py
+++ requests/xkcd-api-practice.py
@@ -7,11 +7,44 @@ practice:
 有些編號其實會拿到 404 not found 請注意
 '''
 
+# todo 紀錄 range 用法 兩篇文章
+# todo index item
+
 import requests
 from pathlib import Path
-Path("./images").mkdir(parents=True, exist_ok=True)
+Path("./requests/images").mkdir(parents=True, exist_ok=True)
 
-# https://xkcd.com/614/info.0.json
+# https://xkcd.com/614/info.0.json example
+
+notGetAllImages = True
+imageCount = 1
+imageList = []
+while notGetAllImages:
+    apiLink = f"https://xkcd.com/{imageCount}/info.0.json"
+    res = requests.get(apiLink)
+    fileImgUrl = res.json()['img']
+    fileTitle = res.json()['title']
+    imgItem = {}
+    imgItem['imgUrl'] = fileImgUrl
+    imgItem['name'] = fileTitle
+    imgItem['number'] = imageCount
+    if (res.ok and fileImgUrl):
+        imageList.append(imgItem)
+
+    if (len(imageList) == 10):
+        notGetAllImages = False
+
+    imageCount = imageCount + 1
+
+
+for item in imageList:
+    fileName = f"{item['number']}-{item['name']}.png"
+    resImg = requests.get(item['imgUrl'])
+    with open(Path(f"./requests/images/{fileName}"), 'wb',) as file:
+        file.write(resImg.content)
+
+print(imageList)
+
 
 # 請開始你的練習
 
