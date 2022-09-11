7203441e7ed54f8c6cfd104522271f297eecdba4
fangfelipe <fangfelipe@gmail.com>
2022-09-11 15:08:39 +0800

    update

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
-# def gcd(m, n):
-print(45 % 10)
-print(10 % 5)
-'''
-45,10
-252,105
+def multi_sort(s):
+    return (len(s), s.count("s"))
 
 
-21 * 12 
-,
-21 * 5
-'''
+mylist = ["pen", "pineapple", "pineapple", "applepens", "apple"]
+
+print(sorted(mylist, key=multi_sort, reverse=True))
+mylist.sort(key=multi_sort, reverse=True)
+print(mylist)
+# ['applepens', 'pineapple', 'pineapple', 'apple', 'pen']
