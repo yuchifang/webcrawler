f12b3fc6a9886841382167650e5ef85bd88713df
yuchifang <Dan.Fang@gmail.com>
2022-09-12 09:25:49 +0800

    update

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 def multi_sort(s):
     return (len(s), s.count("s"))
 
+print(set({"a":1,"c":2,"d":6,"w":4,"cc":5}))
+print(set((1,2,3,4,5)))
+print(set([2,3,4,5]))
 
 mylist = ["pen", "pineapple", "pineapple", "applepens", "apple"]
 
