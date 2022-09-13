dc47eeb18d3897b496b3492385460df26eeea326
yuchifang <Dan.Fang@gmail.com>
2022-09-13 15:23:31 +0800

    update

--- BeautifulSoup/main.py
+++ BeautifulSoup/main.py
@@ -1,5 +1,3 @@
-# 字串 % 特別原因
-
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -7,12 +5,14 @@ from bs4 import BeautifulSoup
 response = requests.get("https://travel.ettoday.net/category/%E6%A1%83%E5%9C%92/")
 soup = BeautifulSoup(response.text,"html.parser")
 
+
+
 soup.prettify()
 
-# find 字串的部份
-# find(string="someString")
+# # find 字串的部份
+# # find(string="someString")
 result = soup.find("p")
-print(result)
-print(result.find_parent())
-print(result.find_next_sibling(content="ETtoday旅遊雲"))
+print(result.get_text())
+# print(result.find_parent())
+# print(result.find_next_sibling(content="ETtoday旅遊雲"))
 # re.compile 是甚麼
