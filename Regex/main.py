e6499d4ce357f836e6efcfdba369012f15c8e634
yuchifang <Dan.Fang@gmail.com>
2022-09-13 17:55:29 +0800

    update

--- Regex/main.py
+++ Regex/main.py
@@ -5,6 +5,7 @@ p = re.compile('[a-z]+')
 result=p.match("8")
 print(result)
 
+# todo 從 Matching Characters 開使看
 '''
 [abc] == [a-c]
 [a-z] 表式 全部英文小寫
@@ -12,9 +13,26 @@ print(result)
 [^5] 會 match 任何值 除了 5
 ^ 這個符號沒有任何特別意思 
 [5^] 會match "5","^"
+
+測試 下面的東西 
+如果要配對特殊符號 要先在前面 加上 "\" 移除他的 特殊規則
+\d
+Matches any decimal digit; this is equivalent to the class [0-9].
+\D
+Matches any non-digit character; this is equivalent to the class [^0-9].
+\s
+Matches any whitespace character; this is equivalent to the class [ \t\n\r\f\v].
+\S
+Matches any non-whitespace character; this is equivalent to the class [^ \t\n\r\f\v].
+\w
+Matches any alphanumeric character; this is equivalent to the class [a-zA-Z0-9_].
+\W
+Matches any non-alphanumeric character; this is equivalent to the class [^a-zA-Z0-9_].
+
+看看放到 [] 裡的差別
 '''
 
-
+# 看看 backslash 怎麼唸
 
 
 
