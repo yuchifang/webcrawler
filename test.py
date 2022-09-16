06da5d49776a6a7fcfcbd5f354c22c5b3f217d5c
yuchifang <Dan.Fang@gmail.com>
2022-09-16 15:03:10 +0800

    update test

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-inputStr = input()
-Newlist=["殘暴戰狼"、"伏特加"、"白金之星"]
-if inputStr in  Newlist:
-	print(inputStr)
-else:
-	print("沒反應")
+def square_check(x):
+	if x ==1:
+		print(1)
+		return 1
+	for item in range(1,x):
+		if item*item >= x:
+			print((item-1)*(item-1))
+			return (item-1)*(item-1)
+
+n = int(input())
+square_check(n)
