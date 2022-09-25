77f64b0917b108388449e7af3995f1cec22abdfd
fangfelipe <fangfelipe@gmail.com>
2022-09-25 22:56:50 +0800

    update

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-thisdict ={}
-thisdict["abc","cc"] = 10 # key 為 tuple
-print(thisdict)
+# 陳詩雅,152,O 潘姿怡,157,AB 林亭莉,161,B
+data = [item.split(",") for item in input().split()]
+input2 =input()
+for item in data:
+    if input2==item[0]:
+        print([item[1],item[2]])
