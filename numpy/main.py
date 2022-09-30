c34f1504559853e94255b25005e1fd6d1db7d9d5
yuchifang <Dan.Fang@gmail.com>
2022-09-30 10:43:07 +0800

    update percentile

--- numpy/main.py
+++ numpy/main.py
@@ -11,12 +11,18 @@
 
 # ddof=1，回傳 sample standard deviation 樣本標準差
 # ，分母(n-1)，無偏估計
-import numpy as np
-arr = np.array([5,6,8,9])
-print(np.std(arr,ddof=1))
-# 樣本去估計整體實際的標準差，就應該使用 ddof=1。
+# import numpy as np
+# arr = np.array([5,6,8,9])
+# print(np.std(arr,ddof=1))
+# # 樣本去估計整體實際的標準差，就應該使用 ddof=1。
 
-# 變異數 
-nums = [5,6,8,9]
-data = np.var(nums,ddof=1)
-print("data",data)
+# # 變異數 
+# nums = [5,6,8,9]
+# data = np.var(nums,ddof=1)
+# print("data",data)
+
+# 百分位數 
+# import numpy as np
+# nums = [20,22,21,19,5,18,20,22,17,19,23,23,22,17,19,20,19,22,18,23]
+# data = np.percentile(nums,75)
+# print("data",data)
