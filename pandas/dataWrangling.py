39424a3e67368ceaa80b26ff9c4895981d42b408
fangfelipe <fangfelipe@gmail.com>
2022-10-05 22:16:12 +0800

    update

--- pandas/dataWrangling.py
+++ pandas/dataWrangling.py
@@ -1,41 +1,46 @@
 # import pandas as pd
-# name = ["Dan","Jack","Cindy","Daniel","Kripp"]
-# occupation = ["enginner","chef","PM","RD","sales"]
+# left = pd.DataFrame({'key1': ['K0', 'K0', 'K1', 'K2'], 
+#                  'key2': ['K0', 'K1', 'K0', 'K1'],
+#                  'A': ['A0', 'A1', 'A2', 'A3'],
+#                  'B': ['B0', 'B1', 'B2', 'B3']})
+# right = pd.DataFrame({'key1': ['K0', 'K1', 'K1', 'K2'],
+#                       'key2': ['K0', 'K0', 'K0', 'K0'],
+#                       'C': ['C0', 'C1', 'C2', 'C3'],
+#                       'D': ['D0', 'D1', 'D2', 'D3'],
+#                       'E': [1,2,3,4]})
 
-# # 建立 dict
-# job_dict = {"name": name,
-#                   "occupation": occupation
-# }
 
-# # 建立第一個 data frame
-# job_df = pd.DataFrame(job_dict)
+# # data2 = pd.merge(left,right)
+# # print(data2)
+# # left
+# '''
+#   key1 key2   A   B
+# 0   K0   K0  A0  B0
+# 1   K0   K1  A1  B1
+# 2   K1   K0  A2  B2
+# 3   K2   K1  A3  B3
+# '''
+# # right
+# '''
+#   key1 key2   C   D  E
+# 0   K0   K0  C0  D0  1
+# 1   K1   K0  C1  D1  2
+# 2   K1   K0  C2  D2  3
+# 3   K2   K0  C3  D3  4
+# '''
 
-# name = ["Dan","Jack","Cindy","Daniel","Kripp","Alan","Leo"]
-# age = ["10", "20", "30", "40", "45","50","15"]
-
-# # 建立 dict
-# age_dict = {"name": name,
-#             "age": age
-#             }
-
-# # 建立第二個 data frame
-# age_df = pd.DataFrame(age_dict)
-
-# # 連接
-# age_job_merged = pd.merge(job_df, age_df)
-# print(age_job_merged)
-# print("---------------------")
-# data = pd.merge(job_df, age_df,left_on="occupation",right_index=True)
+# data =pd.merge(left, right, right_on=['E'], left_index = True, how = 'outer')  
 # print(data)
+
 import pandas as pd
 listOne = range(3)
 listTwo = ["A","B","C"]
 df1 = pd.DataFrame({"data2":listOne,"name":listTwo})
-# print(df1)
+print(df1)
 listT = range(5,0,-1)
 listF = ["A","B","C","D","C"]
 df2 = pd.DataFrame({"data1":listT,"name":listF})
-# print(df2)
-df3 = pd.merge(df1,df2,right_on="data1",left_index=True)
+print(df2)
+df3 = pd.merge(df1,df2,left_index=True,right_on="data1")
 # df3 = pd.merge(df1,df2,left_on="data2",right_index=True)
 print(df3)
