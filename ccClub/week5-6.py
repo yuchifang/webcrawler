15dfb47b39d1ccd138714cb9f569f4f9c2e55687
fangfelipe <fangfelipe@gmail.com>
2022-10-17 23:37:56 +0800

    update

--- ccClub/week5-6.py
+++ ccClub/week5-6.py
@@ -11,8 +11,28 @@ end
 "「"
 "」"
 
-input1 = input()
+'''
+test case 1
+
+apple app cute
+I have an apple
+you are so cute
+end
+
+test case 2
+開心 快樂 愉悅
+今天的你不得不快樂
+快樂且愉悅
+end
+
+'''
+input1 = input().split()
+dictItem = {}
+for item in input1:
+    dictItem[item] = "「" + item+"」"
+
 data = ""
 while data != "end":
     data = input()
-    print(data)
+    for item in dictItem:
+        print(data.replace(item, dictItem[item]))
