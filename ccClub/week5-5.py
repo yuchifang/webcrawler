a4af967d81288bdf1c856040c2f7c6a50601a2af
yuchifang <Dan.Fang@gmail.com>
2022-10-20 17:07:29 +0800

    update

--- ccClub/week5-5.py
+++ ccClub/week5-5.py
@@ -33,59 +33,90 @@
 
 # 想想有沒有更快的解法 一次迴圈?
 # Time Limit Exceeded
-input1 = [item for item in input()]
-
-totalInputLength = len(input1)
-def isValid(input1):
-    temp = 0
-    indexCount = 0
-    # 從頭開始找值
-    for item in input1:
-        # 從尾開始找值
-        for insideIndexCompare in range(totalInputLength-1, indexCount, -1):
-            input2 = input1.copy()
-            # 如果尾比頭大 交換
-            if input1[insideIndexCompare] > item:
-                input2[indexCount] = input2[insideIndexCompare]
-                input2[insideIndexCompare] = item
-                data = int("".join(input2))
-                # 紀錄值替換後的值 為了此內層for比大小用
-                if data > temp:
-                    temp = data
-        if temp != 0:
-            return temp
-        indexCount += 1
+# todo
+# input1 = [int(item) for item in input()]
 
 
-data = isValid(input1)
-if data:
-    print(data)
-else:# 沒找到 print 原字串
-    print("".join(input1))
+# totalInputLength = len(input1)
+# def isValid(input1):
+#     temp = 0
+#     indexCount = 0
+#     # 從頭開始找值
+#     for item in input1:
+#         # 從尾開始找值
+#         for insideIndexCompare in range(totalInputLength-1, indexCount, -1):
+#             input2 = input1.copy()
+#             # 如果尾比頭大 交換
+#             if input1[insideIndexCompare] > item:
+#                 input2[indexCount] = input2[insideIndexCompare]
+#                 input2[insideIndexCompare] = item
+#                 data = int("".join(input2))
+#                 # 紀錄值替換後的值 為了此內層for比大小用
+#                 if data > temp:
+#                     temp = data
+#         if temp != 0:
+#             return temp
+#         indexCount += 1
 
 
-# other guy answer
-# [*input()] ??
-# lIndex[lIndex],lIndex[RIndex]=input1[RIndex],input1[lIndex]
+# data = isValid(input1)
+# if data:
+#     print(data)
+# else:# 沒找到 print 原字串
+#     print("".join(input1))
 
 
-input1 = [int(x) for x in[*input()]]
-tMax = -1
-tMaxIndex = -1
-lIndex = -1
-RIndex = 0
+# # other guy answer
+# # [*input()] ??
+# # lIndex[lIndex],lIndex[RIndex]=input1[RIndex],input1[lIndex]
 
 
-for index in range(len(input1)-1,-1,-1):
-    if input1[index] > tMax:
-        tMax = input1[index]
-        tMaxIndex = index
-        continue
-    if input1[index]<tMax:
-        lIndex = index
-        RIndex=tMaxIndex
-if lIndex ==-1:
-    print(int("".join(map(str,input))))
-else:
-    lIndex[lIndex],lIndex[RIndex]=input1[RIndex],input1[lIndex]
-    print(int(''.join(map(str,input1))))
+# input1 = [int(x) for x in[*input()]]
+# tMax = -1
+# tMaxIndex = -1
+# lIndex = -1
+# RIndex = 0
+
+
+# for index in range(len(input1)-1,-1,-1):
+#     if input1[index] > tMax: # 從找尾最大值 
+#         tMax = input1[index]
+#         tMaxIndex = index
+#         continue
+#     if input1[index]<tMax: # 從 比最大值小的值
+#         lIndex = index
+#         RIndex=tMaxIndex
+# if lIndex ==-1:
+#     print(int("".join(map(str,input))))
+# else:
+#     input1[lIndex],input1[RIndex]=input1[RIndex],input1[lIndex]
+#     print(int(''.join(map(str,input1))))
+
+
+
+
+input1 = [int(item) for item in input()]
+maxValue =-1
+maxIndex = 0
+maxValueMinIndex = -1
+minValue=0
+for item in input1:
+    if maxValue <= item and maxIndex != 0 and max(input1)>item:
+        maxValue = item
+        maxValueMinIndex = maxIndex
+    maxIndex +=1
+for index in range(maxValueMinIndex):
+    if input1[index] < maxValue:
+        input1[index],input1[maxValueMinIndex] =maxValue,input1[index]
+        break
+print("".join([str(item) for item in input1]))
+'''
+9876 = 9876
+3578 = 8573
+102030405060708090 = 902030405060708010
+991199 = 999191
+498712301 = 948712301 
+988588011 = 988885011
+98769 = 99768
+997788
+'''
--- test.py
+++
@@ -1,3 +0,0 @@
-print([*"asd"])
-# https://kknews.cc/zh-tw/code/lj5veag.html
-# https://www.php.cn/python-tutorials-474944.html
