8533108e9d12a1a9868b15be0a223b3b918cfdf1
yuchifang <Dan.Fang@gmail.com>
2022-10-26 17:50:38 +0800

    update

--- ccClub/w7/w7-1.py
+++ ccClub/w7/w7-1.py
@@ -8,15 +8,32 @@
 1 2 5 6 9
 3
 '''
-# 找到所有值的差 
-# 比較哪個最多
-count = 0
+data = [int(item) for item in input().split()]
+for item in data:
+    # 用物件記錄 key 為差值 value 為陣列
+    pass
+
+
+# todo 解法一
+'''
+count = 1
 countList = []
 data = [int(item) for item in input().split()]
 dataLength = len(data)
+dicDiff={}
 dicNumber={}
 for index in range(dataLength):
+    dicNumber[data[index]]=data[index]
     for index2 in range(index+1,dataLength):
-        dicNumber[data[index2]-data[index]]=data[index2]-data[index]
-for 
-print(dicNumber)
+        dicDiff[data[index2]-data[index]]=data[index2]-data[index]
+for item in dicDiff:
+    for itemNumber in dicNumber:
+        key = itemNumber +item
+        while key in dicNumber:
+            key+=item
+            count+=1
+            countList.append(count)
+        count =1
+
+print(max(countList))
+'''
