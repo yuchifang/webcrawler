7439e3f213e173ed36b4b5d36cca49019a55fc08
yuchifang <Dan.Fang@gmail.com>
2022-10-27 11:51:02 +0800

    update

--- ccClub/w7/w7-3.py
+++ ccClub/w7/w7-3.py
@@ -15,3 +15,32 @@ N
 Y 停車證 10元
 Y less 30 minute free
 '''
+startTime = [int(item)for item in input().split(":")]
+endTime = [int(item)for item in input().split(":")]
+haveCard = input()
+if haveCard =="Y":
+    haveCard = True
+else:
+    haveCard = False
+startTimeMinute = startTime[0] * 60 + startTime[1]
+endTimeMinute = endTime[0] * 60 + endTime[1]
+
+def HowMuchToPay(startTimeMinute,endTimeMinute,haveCard):
+    totalTime = endTimeMinute - startTimeMinute
+    if totalTime == 0:
+        return 0
+    if haveCard and totalTime < 30:
+        return 0
+    
+    add = totalTime % 30
+    count = totalTime // 30
+    if add != 0:
+        count += 1
+
+    if haveCard:
+        return count * 10
+    else:
+        return count * 20
+
+
+print(int(HowMuchToPay(startTimeMinute,endTimeMinute,haveCard)))
---
+++ ccClub/w7/w7-4.py
@@ -0,0 +1,76 @@
+'''
+
+舉例來說：玩家可以消費 350 金幣購買一個把長劍，消費 450 金幣購買一個抗魔斗篷，並且額外花費 600 金幣將兩者合成為吸魔劍，總共花費 1400 金幣。
+
+輸入的第一部分為取得公式，其中第一行n為取得公式的數量，以下為n行取得公式。
+[裝備] [金額] [所需裝備1] [所需裝備2]
+若該道具可以直接購買，則所需裝備的位置會留空
+
+輸入的第二部分為想取得的道具，其中第一行 m 為想取得的道具數量，以下 m 行各包含一個字串為道具名稱。
+道具的取得之間是獨立的，每一次所要花費的金幣並不會因為你上一次取得了這次所需的道具而改變。
+輸出包含m行，每行為各個想購買的道具的總價。
+
+
+輸入的第一部分為 n+1 行，其中第一行 n 為取得公式的數量，以下為 n 行取得公式。
+取得裝備的公式格式如下：[裝備] [金額] [所需裝備1] [所需裝備2]...
+
+輸入的第二部分為 m+1 行，其中第一行 m 為想取得的道具數量，
+以下 m 行各包含一個字串為道具名稱。
+輸出包含m行，每行為各個想購買的道具的總價。
+
+3
+長劍 350
+抗魔斗篷 450
+吸魔劍 600 長劍 抗魔斗篷
+2
+長劍
+吸魔劍
+
+350
+1400
+
+
+
+2
+a 100 b b b
+b 1
+2
+a
+b
+
+103
+1
+
+'''
+menu = {}
+input1 = int(input())
+for item in range(input1):
+    data = input().split()
+    if len(data) == 2:
+        menu[data.pop(0)] = data
+    else:
+        menu[data.pop(0)] = data
+
+input2 = int(input())
+
+totalWantItem = []
+for item in range(input2):
+    data = input()
+    totalWantItem.append(menu[data])
+for item in totalWantItem:
+
+    if len(item) >=2:
+        add = int(item.pop(0))
+        for data in item:
+            add += int(menu[data][0])
+        print(add)
+
+    else:
+        print(int(item[0]))
+
+
+        
+
+
+
+
