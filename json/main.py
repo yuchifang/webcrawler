6201f12f9c9814ddd512a47e83822a62d292ba59
fangfelipe <fangfelipe@gmail.com>
2022-11-15 06:42:02 +0800

    update

---
+++ json/main.py
@@ -0,0 +1,5 @@
+import json
+d = {"kevin": "wei", "kt": "tsai", "Wayne": "Huang"}
+
+# dict 轉字串
+print(json.dumps(d))
--- test.py
+++
@@ -1,6 +0,0 @@
-keys = ['a', 'b', 'c']
-vals = [1, 2, 3]
-zipped = zip(keys, vals)
-print(dict(zipped))
-
-print(dict(zipped))
