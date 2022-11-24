b872a645fad51edbcc5bc9869c72c05d4708fcc9
fangfelipe <fangfelipe@gmail.com>
2022-11-24 20:53:55 +0800

    update

--- pandas/main.py
+++ pandas/main.py
@@ -5,17 +5,18 @@ groups = ["Modern Web", "DevOps", "Cloud", "Big Data", "Security", "自我挑戰
 ironmen = [59, 9, 19, 14, 6, 77]
 
 ironmen_dict = {
-                "groups": groups,
-                "ironmen": ironmen
+    # "groups": groups,
+    "ironmen": ironmen
 }
 
 # 建立 data frame
 ironmen_df = pd.DataFrame(ironmen_dict)
+print(ironmen_df.ndim)  # 2
 
-print(ironmen_df.count()) # 計算總鐵人數
-print("---") # 分隔線
-print(ironmen_df.mean()) # 計算平均鐵人數
-print("---") # 分隔線
-print(ironmen_df.std()) # 計算中位數
-print("---") # 分隔線
-print(ironmen_df.describe()) # 描述統計
+print(ironmen_df.count())  # 計算總鐵人數
+print("---")  # 分隔線
+# print(ironmen_df.mean()) # 計算平均鐵人數
+print("---")  # 分隔線
+# print(ironmen_df.std()) # 計算中位數
+print("---")  # 分隔線
+print(ironmen_df.describe())  # 描述統計
---
+++ pandas/test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+groups = ["Modern Web", "DevOps", "Cloud", "Big Data", "Security", "自我挑戰組"]
+ironmen = [59, 9, 19, 14, 6, 77]
+
+ironmen_dict = {
+    "groups": groups,
+    "ironmen": ironmen
+}
+ironmen_df = pd.DataFrame(ironmen_dict)
+print(ironmen_df)
